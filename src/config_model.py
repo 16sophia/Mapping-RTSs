cd1ad15dc01a3985f60a8b7af9b32358b4e2709c
Sophia Ly <sophia.ly.16@gmail.com>
2024-02-07 14:50:15 +0000

    add model

--- src/config_model.py
+++ src/config_model.py
@@ -105,7 +105,7 @@ def get_config(mean_sd, model = "baseline"):
             nesterov=False, # enables Nesterov momentum
             momentum=0.9, # considering past gradients: set too high-> slow convergence /divergence. Set too low-> no significant improvements over standard
             weight_decay=0.001, #  L2 regularization (discourages large weights): loss = loss + weight decay parameter * L2 norm of the weights
-            optimizer_config = "adam", # adam/ sgd
+            optimizer_config = "adamW", # adam/ sgd
             betas=(0.9, 0.999), # Adam optimizer: coefficients used for computing running averages of gradient and its square 
             eps = 1e-8, #term added to the denominator to improve numerical stability 
             amsgrad = False, # whether to use the AMSGrad variant of this algorithm
@@ -472,6 +472,55 @@ def get_config(mean_sd, model = "baseline"):
         #trainable_backbone_layers: number of trainable layers starting from final block (ranging from 0 - 5)
         pretrained_network = maskrcnn_resnet50_fpn(weights=MaskRCNN_ResNet50_FPN_Weights.DEFAULT, trainable_backbone_layers=3) 
         return config_baseline, device, num_classes, worker_processes, gpu, gpu_device, threshold_mask, pretrained_network, transformed_data
+
+    
+    if model == "final_model":
+        ''''
+        Bsaeline with 0.001 and adamW
+        slope
+        dropout, nms
+        '''
+        config_baseline = dict (
+            project = "RTS_detection",
+            architecture = "maskrcnn_resnet50_fpn",
+            dataset_id = "scaled_DEM_difference",
+            lr=0.0001, # step size at which the model's weights are updated usually [0,1]
+            min_lr=0.0000001, # minimum learning rate if learning rate gradually reduces 
+            epochs=150, # number of times the model will process the entire training dataset
+            batch_size= 5, # number of data samples processed together in each training iteration (forward and backward pass)
+            nesterov=False, # enables Nesterov momentum
+            momentum=0.9, # considering past gradients: set too high-> slow convergence /divergence. Set too low-> no significant improvements over standard
+            weight_decay=0.001, #  L2 regularization (discourages large weights): loss = loss + weight decay parameter * L2 norm of the weights
+            optimizer_config = "adamW", # adam/ sgd
+            betas=(0.9, 0.999), # Adam optimizer: coefficients used for computing running averages of gradient and its square 
+            eps = 1e-8, #term added to the denominator to improve numerical stability 
+            amsgrad = False, # whether to use the AMSGrad variant of this algorithm
+            hidden_layer=256,
+            n_channel = 2,
+            channel_list = ["images", "slope"], 
+            img_mean = [mean_sd['img_mean'][0],mean_sd['slope_mean'][0]],
+            img_std = [mean_sd['img_sd'][0],mean_sd['slope_sd'][0]],
+            img_size = 256,
+            min_box_axis = 1,
+            lrscheduler = "onplateau",
+            drop_out_apply = True,
+            freeze_layer = False,
+            adapt_nms = True,
+            post_nms_top_n = {'training': 700, 'testing': 250},
+            rpn_nms_thresh = 0.5,
+            box_nms_thresh = 0.25)
+                  
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        num_classes = 2 # background, RTS
+        worker_processes = 2 
+        gpu=  torch.cuda.is_available() # for trainer definition
+        gpu_device = torch.cuda.device_count() # for trainer definition
+        threshold_mask = 0.5 # threshold for preformance metric during testing
+        transformed_data = True
+        change_min_box_axis = True
         
+        #trainable_backbone_layers: number of trainable layers starting from final block (ranging from 0 - 5)
+        pretrained_network = maskrcnn_resnet50_fpn(weights=MaskRCNN_ResNet50_FPN_Weights.DEFAULT, trainable_backbone_layers=5) 
+        return config_baseline, device, num_classes, worker_processes, gpu, gpu_device, threshold_mask, pretrained_network, transformed_data, change_min_box_axis     
     else:
         print("not a valid model config")
